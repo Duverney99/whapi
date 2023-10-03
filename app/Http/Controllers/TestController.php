f1236d598a4d547106adf88dbeb4968c5d27e68f
duberney <duberneyhernandez@uts.edu.co>
2023-10-03 10:40:57 -0500

    0.1

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,9 +2,49 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class TestController extends Controller
 {
-    //
+    public function sedMensad()
+    {
+        try{
+            $token = 'EAATd9zYLfFoBOxbQaB0ZCdrBBWR1JDhxoU9n9CrldymMOZBpBkgZCXZATM6iG162aehrGeP860v46XKa16LsGAv8TvVXZCGtMETmDPHyVwBa13LkmgSZAfhQMWEwNCn0xpIRDirgccvzsMmLOGB1DgKaQanareNohiHaBQKnbHMGZC32ykhGudeKkmeiMvzjqaEl7pbCCYhk5PWdEafVdAZD';
+        $phoneId ='153886304464407';
+        $version = 'v17.0';
+        $payload = [
+            'messaging_product' => 'whatsapp',
+            'to' => '573024709509',
+            'type' => 'template',
+            "template" => [
+                "name" => "hello_world",
+                "language" => [
+                    "code" => "en_US"
+                ]
+
+            ]
+
+
+        ];
+        
+        $message = Http::withToken($token)->post('https://graph.facebook.com/' . $version . '/'  . $phoneId . '/messages', $payload)->throw()->json();
+
+        return response()->json([
+            'success' => true,
+            'data' => $message,
+           ],200);
+
+        }
+        catch (Exception $e){
+            return response()->json([
+                'success' => true,
+                'error' => $e->getMessage(),
+            
+            ],500);
+        }
+        
+
+    }
 }
